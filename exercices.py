e66741513501a715fbdcf62008f5556e0832cde2
Adri <adrien.baglin@gmail.com>
2021-09-28 15:52:08 +0200

    210921

--- exercices.py
+++ exercices.py
@@ -17,44 +17,38 @@
 # instMeth(list,8)
 
 
-list = ["a","a","a","b","b","b","c","c","c"]
-def remStr(p):
-    list.pop(p)
-    a =  0
-    b =  0
-    c =  0
-    for e in range (int(len(list))):
-        if(list[e] == "a"):
-            a+=1
+# list = ["a","a","a","b","b","b","c","c","c"]
+# def remStr(p):
+#     list.pop(p)
+#     a =  0
+#     b =  0
+#     c =  0
+#     for e in range (int(len(list))):
+#         if(list[e] == "a"):
+#             a+=1
             
-        if(list[e] == "b"):
-            b+=1
+#         if(list[e] == "b"):
+#             b+=1
             
-        if(list[e] == "c"):
-            c+=1
-    
-    while find:
+#         if(list[e] == "c"):
+#             c+=1
+
+#     if(a > b):
+#         print(f"il reste {b} fois lettre b")
+#     elif(b > a):
+#         print(f"il reste {a} fois lettre a")
+#     else:
+#         print(f"il reste {c} fois lettre c")
+
+# remStr(8)
+
+listA = ["1","2","3","4","5"]
+listB = ["1","3","5","7","9"]
+listC = ["0","2","4","6","8"]
+
+def trList(la,lb):
+    for a in range (len(la)):
         
+            
 
-
-
-    if(0 < a):
-        text = f"il reste {a} fois lettre a"
-    elif(0 < b):
-        text = f"il reste {a} fois lettre a"
-    elif(0 < c):
-        text = f"il reste {a} fois lettre a"
-
-
-
-    print(a)
-    print(b)
-    print(c)             
-    if(0 < a):
-        print(f"il reste {a} fois lettre a")
-    elif(0 < b):
-        print(f"il reste {b} fois lettre a")
-    elif(0 < c):
-        print(f"il reste {c} fois lettre a")
-
-remStr(5)
+trList(listA,listB)
